360a2d795328a116dc360051b349435fdaa3bfc0
Nishikawa5 <gustavo.nishikawa1@gmail.com>
2024-10-31 16:01:15 -0300

    codevita

--- Codevita/Training/WaterCistern.cpp
+++ Codevita/Training/WaterCistern.cpp
@@ -2,6 +2,32 @@
 
 using namespace std;
 
+#define PI 3.14159265358979323
+
+double calculate_x_curve(int r, int deg) {
+    return 2 * PI * r * (min(deg, 360 - deg) / 360.0);
+}
+
 int main(void) {
-    
+    int rad, height, start;
+    int dist, deg;
+    scanf("%d,%d,%d", &rad, &height, start);
+    scanf("%d,%d", &dist, &deg);
+
+    double min_dist;
+    if (dist < 0) {
+        // calculate 
+
+        // go to the top
+        // test every possible degree and get the min
+        for (int dg_test = 0; dg_test < 360; dg_test++) {
+
+        }
+    } else {
+        // curve
+        double dist_x = calculate_x_curve(rad, deg);
+        double dist_y = dist - start;
+        min_dist = sqrt(dist_x * dist_x + dist_y * dist_y);
+    }
+
 }
