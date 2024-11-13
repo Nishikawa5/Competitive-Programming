9202b889626e224c7dffe21103550ecf3342bb61
Gustavo Nishikawa <gustavo.nishikawa1@gmail.com>
2024-11-13 19:17:38 -0300

    goodstring.cpp fix codevita

--- Codevita/Practice/Practice_2024/goodstring.cpp
+++ Codevita/Practice/Practice_2024/goodstring.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// could have used lowerbound instead
 int first_true(int lo, int hi, function<bool(int)> f) {
     hi++;
 
@@ -20,9 +21,9 @@ int main() {
     string goodstring, name;
     cin >> goodstring >> name;
 
-    sort(goodstring.begin(), goodstring.end());
+    char last_char = goodstring[0];
 
-    cout << goodstring << endl;
+    sort(goodstring.begin(), goodstring.end());
 
     int min_dist = 0;
     for (char c: name) {
@@ -32,12 +33,24 @@ int main() {
         });
 
         if (first_greater == 0) {
+            // no smaller char to the left
             min_dist += abs(goodstring[first_greater] - c);
         } else if (first_greater == goodstring.size()) {
+            // no bigger char to the right
             min_dist += abs(goodstring[first_greater - 1] - c);
         } else {
-            min_dist += min(abs(goodstring[first_greater] - c), abs(goodstring[first_greater - 1] - c));
+            // check if the distances between left and right are equal
+            if (abs(goodstring[first_greater] - c) == abs(goodstring[first_greater - 1] - c)) {
+                // equal distances
+                min_dist += min(abs(last_char - goodstring[first_greater]), 
+                                abs(last_char - goodstring[first_greater - 1]));
+            } else {
+                // not equal, just add
+                min_dist += min(abs(goodstring[first_greater] - c), 
+                                abs(goodstring[first_greater - 1] - c));
+            }
         }
+        last_char = goodstring[first_greater];
     }
     cout << min_dist << endl;
 }
