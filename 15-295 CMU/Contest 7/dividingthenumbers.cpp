1d9f126eb01d42069b1e85004267698d1df998d2
Gustavo Nishikawa <gustavo.nishikawa1@gmail.com>
2025-06-15 18:38:59 -0300

    feat: add solutions to Contest 7

--- 15-295 CMU/Contest 7/dividingthenumbers.cpp
+++ 15-295 CMU/Contest 7/dividingthenumbers.cpp
@@ -5,22 +5,49 @@ void solve() {
     int n;
     cin >> n;
 
-    if (n == 2) {
-        cout << 1 << endl;
-        cout << 1 << " " << 1;
-        return;
-    } else if (n % 2) {
-        cout << n/2 + 1 << endl;
+    /*
+    Separate the numbers in 2 parts:
+        left    0 < len <  4      
+            for len == 1 => 1
+                len == 2 => 1
+                len == 3 => 0
+
+        right   len % 4 == 0      == 0
+    */
+
+    vector<int> rest = {0, 1, 1, 0};
+    int printed_numbers = 0;
+
+    int left_part = n % 4;    
+    int right_part_num = (n - left_part) / 2;
+
+    cout << rest[n % 4] << endl;
+
+    // for 0 do nothing, since we just output right part
+    int extra = n % 4;
+    if (n % 4 == 1) {
+        cout << right_part_num;
+         
+    } else if (n % 4 == 2) {
+        // do the 2 case
+        cout << right_part_num + 1;
+        cout << " 1";
+    } else if (n % 4 == 3) {
+        // do the 3 case
+        cout << right_part_num + 1;
+        cout << " 3";
     } else {
-        cout << 0 << endl;
+        cout << right_part_num;
     }
 
-    cout << n/2;
+    // right_part_num always even now
     int idx = 1;
-    for (int i = 1; i <= n / 2; i += 2) {
-        cout << " " << idx;
+    for (int i = 0; i < right_part_num; i += 2) {
+        cout << " " << idx + extra;
         cout << " " << n - idx + 1;
+        idx++;
     }
+
     cout << endl;
 }
 
